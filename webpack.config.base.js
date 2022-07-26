92f5d4f06874baf0e2aabb359856aaee6046cc07
ddaisylee <elliejay9988@gmail.com>
2022-07-27 00:45:20 +0900

    dependencies 추가 및 설정

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,30 +2,25 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'docs'),
-    filename: "app.bundle.js"
+    path: path.join(__dirname, "docs"),
+    filename: "app.bundle.js",
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
+        use: "babel-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        exclude: /node_modules/,
         use: ["style-loader", "css-loader"],
+        exclude: /node_modules/
       }
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
